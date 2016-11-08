d33e559223925d8df239c62d95c2ef5a52fdf0dc
Mikhail Snitovets <mksn13@gmail.com>
2016-11-08 13:09:44 +0300

    Solution added, view and clicker added

---
+++ WebCrawler/WebCrawler/ViewModel/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WebCrawler.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private Action execute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler/ViewModel/WebCrawlerViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WebCrawler.ViewModel
+{
+    class WebCrawlerViewModel : BaseViewModel
+    {
+
+        public WebCrawlerViewModel()
+        {
+            _clickerCommand = new RelayCommand(() =>
+            {
+                _clickerCount++;
+                OnPropertyChanged("ClickerValue");
+            });
+            _clickerResetCommand = new RelayCommand(() =>
+            {
+                _clickerCount = 0;
+                OnPropertyChanged("ClickerValue");
+            });
+        }
+
+        public string ClickerValue
+        {
+            get
+            {
+                return (_clickerCount == 0) ? "Click Me!" : Convert .ToString(_clickerCount);
+            }
+        }
+
+        public ICommand ClickerCommand
+        {
+            get
+            {
+                return _clickerCommand;
+            }
+        }
+
+        public ICommand ClickerResetCommand
+        {
+            get
+            {
+                return _clickerResetCommand;
+            }
+        }
+
+        private int _clickerCount = 0;
+        private ICommand _clickerCommand;
+        private ICommand _clickerResetCommand;
+
+    }
+}
