c77b9133bac8fb23cd925e0af6f65bd0fb092623
Mikhail Snitavets <mksn13@gmail.com>
2016-11-13 22:24:26 +0300

    XmlCrawlerConfig added

---
+++ WebCrawler/WebCrawler/Model/CrawlConfigException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WebCrawler.Model
+{
+    [Serializable]
+    internal class CrawlConfigException : Exception
+    {
+        public CrawlConfigException()
+        {
+        }
+
+        public CrawlConfigException(string message) : base(message)
+        {
+        }
+
+        public CrawlConfigException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CrawlConfigException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler/Model/XmlCrawlerConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using WebCrawlerCore;
+
+namespace WebCrawler.Model
+{
+    class XmlCrawlerConfig : ICrawlerConfig
+    {
+
+        private readonly int CrawlDepth;
+        private readonly string[] RootUrls;
+
+        public XmlCrawlerConfig(string filename)
+        {
+            try
+            {
+                XDocument document = XDocument.Load(filename);
+                XElement root = document.Root;
+
+                CrawlDepth = int.Parse(root.Element("depth").Value);
+                RootUrls = root.Element("rootResources")
+                        .Elements("resource")
+                        .Select(x => x.Value)
+                        .Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            }
+            catch (Exception e)
+            {
+                throw new CrawlConfigException("Something wrong with config.", e);
+            }
+        }
+
+        public int GetCrawlDepth()
+        {
+            return CrawlDepth;
+        }
+
+        public string[] GetRootUrls()
+        {
+            return RootUrls;
+        }
+    }
+}
